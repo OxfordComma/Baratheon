75f18c481b08fd94ba7455e8da4cd57cb9cacf18
Nick Lima <nick@Nicks-MacBook-Pro.local>
2016-09-24 23:32:27 -0400

    Using Unity lobby manager from docs

--- Assets/Editor/SceneLoader.cs
+++ Assets/Editor/SceneLoader.cs
@@ -5,8 +5,8 @@ using UnityEditor.SceneManagement;
 public class SceneLoader : MonoBehaviour
 {
 
-    [MenuItem("Scenes/Open Main Scene %l")]
-    static void OpenMainScene()
+	[MenuItem("Scenes/Open Scene 1 &1")]
+    static void OpenScene1()
     {
         if (EditorApplication.isPlaying == true)
         {
@@ -15,6 +15,45 @@ public class SceneLoader : MonoBehaviour
         }
         EditorSceneManager.SaveCurrentModifiedScenesIfUserWantsTo();
         EditorSceneManager.OpenScene("Assets/Scenes/Login Screen.unity");
-        EditorApplication.isPlaying = true;
+//        EditorApplication.isPlaying = true;
     }
+
+	[MenuItem("Scenes/Open Scene 2 &2")]
+	static void OpenScene2()
+	{
+		if (EditorApplication.isPlaying == true)
+		{
+			EditorApplication.isPlaying = false;
+			return;
+		}
+		EditorSceneManager.SaveCurrentModifiedScenesIfUserWantsTo();
+		EditorSceneManager.OpenScene("Assets/Scenes/Main Menu.unity");
+//		EditorApplication.isPlaying = true;
+	}
+
+	[MenuItem("Scenes/Open Scene 3 &3")]
+	static void OpenScene3()
+	{
+		if (EditorApplication.isPlaying == true)
+		{
+			EditorApplication.isPlaying = false;
+			return;
+		}
+		EditorSceneManager.SaveCurrentModifiedScenesIfUserWantsTo();
+		EditorSceneManager.OpenScene("Assets/Scenes/Build Deck.unity");
+//		EditorApplication.isPlaying = true;
+	}
+
+	[MenuItem("Scenes/Open Scene 4 &4")]
+	static void OpenScene4()
+	{
+		if (EditorApplication.isPlaying == true)
+		{
+			EditorApplication.isPlaying = false;
+			return;
+		}
+		EditorSceneManager.SaveCurrentModifiedScenesIfUserWantsTo();
+		EditorSceneManager.OpenScene("Assets/Scenes/Battlefield.unity");
+//		EditorApplication.isPlaying = true;
+	}
 }
