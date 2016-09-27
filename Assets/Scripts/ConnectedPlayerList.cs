fbb64d1c29f7cd858498bf0f03082319d2fea116
Nick Lima <Nick Lima>
2016-09-26 20:52:20 -0400

    Round 3

---
+++ Assets/Scripts/ConnectedPlayerList.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using UnityEngine.Networking;
+using UnityEngine.UI;
+using System.Collections;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+
+public class ConnectedPlayerList : NetworkBehaviour {
+    Text text;
+	// Use this for initialization
+	void Start () {
+        text = this.GetComponent<Text>();
+
+        foreach (NetworkConnection n in NetworkServer.connections)
+        {
+            if (n == null)
+                continue;
+            Debug.Log(n.playerControllers[0].gameObject.name);
+        }
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        
+        
+    }
+}
